f40c7dcb1e1624d286e6e9dba5cc26abb64b27fa
Jhon Loyd <jhonloydpastorin.03@gmail.com>
2025-07-26 14:41:55 +0800

    refactor code

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,29 +1,29 @@
-const ShopifyService = require('./ShopifyService');
+const ShopifyProductService = require('./ShopifyProductService');
 
 class ProductService {
   static async getAllProducts({ limit = 10, offset = 0 }) {
     // Fetch all products from Shopify
-    return await ShopifyService.getAllProducts({ limit, offset });
+    return await ShopifyProductService.getAllProducts({ limit, offset });
   }
 
   static async getProductById(id) {
     // Fetch a single product from Shopify
-    return await ShopifyService.getProductById(id);
+    return await ShopifyProductService.getProductById(id);
   }
 
   static async createProduct(input) {
     // Create product in Shopify only
-    return await ShopifyService.createProduct(input);
+    return await ShopifyProductService.createProduct(input);
   }
 
   static async updateProduct(id, input) {
     // Update product in Shopify only
-    return await ShopifyService.updateProduct(id, input);
+    return await ShopifyProductService.updateProduct(id, input);
   }
 
   static async deleteProduct(id) {
     // Delete product in Shopify only
-    return await ShopifyService.deleteProduct(id);
+    return await ShopifyProductService.deleteProduct(id);
   }
 }
 
